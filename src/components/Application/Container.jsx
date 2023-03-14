868100f8bd8ed3b66008535ce3cd6d1e91014cc5
Hüseynağa Haqverdiyev <huseynagahaqverdiyev@Huseynagas-MacBook-Pro.local>
2023-03-14 23:06:56 +0400

    deploy

--- src/components/Application/Container.jsx
+++ src/components/Application/Container.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { v4 as uuidv4 } from "uuid";
 import { useEffect, useState, useRef } from "react";
 import Note from "./Note";
 import AllNotes from "./AllNotes";
